f8d3fa6130e3c2ae765a9e1b71a47b35efdb18be
Ian  Bumatay <ianbumatay.09@gmail.com>
2020-02-22 12:48:36 +0000

    Automatically backed up by Learn

--- spec/02_birthday_spec.rb
+++ spec/02_birthday_spec.rb
@@ -27,9 +27,3 @@ describe "#age_appropriate_birthday" do
 
   end
 end
-
-def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts"Happy Birthday #{kids_name}! You are now #{age} years old!"
- end
-end
